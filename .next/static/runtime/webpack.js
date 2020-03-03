827a2d0e8d24ebf57a80cf0464546956925ee911
nguyentruc <truc.nguyen.051594@gmail.com>
2020-03-03 15:22:01 +0700

    add postcss autoprefix

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7f7e56c24f2ec613d633";
+/******/ 	var hotCurrentHash = "f0965af884cec930a7b4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
